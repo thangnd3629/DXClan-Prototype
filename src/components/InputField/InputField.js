521c9934928f6f8d4a20d5b6c1ffee50774acb55
NguyenDucThangNghiemTuc <thang.nd182629@gmail.com>
2021-12-28 15:29:33 +0700

    add build blocks

---
+++ src/components/InputField/InputField.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react"
+import "./InputField.css"
+export class InputField extends Component {
+  state = {
+    input: this.props.value,
+  }
+  inputOnChange = (e) => {
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        input: e.target.value,
+      }
+    })
+  }
+  render() {
+    return (
+      <div className="field">
+        <input
+          type={this.props.type}
+          required
+          disabled={this.props.disabled}
+          value={this.state.input}
+          onChange={(e) => {
+            this.inputOnChange(e)
+            this.props.onChange(this.props.field, e.target.value)
+          }}
+        />
+        <label>{this.props.label}</label>
+      </div>
+    )
+  }
+}
+
+export default InputField
---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Pagination.css'
+import {Link} from 'react-router-dom'
+const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  
+
+  return (
+
+    <div className='pagination'>
+      <ul>
+        {pageNumbers.map(number => (
+          <li key={number} className={number === currentPage ? 'pageNumber active' : 'pageNumber'}>
+            <Link onClick={() => paginate(number)} to='#'>
+              {number}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+
+  );
+};
+
+export default Pagination;
