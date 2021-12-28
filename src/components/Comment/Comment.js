a9995520aa3f9ce0bcdec88b99195c9e66284898
quanhoang288 <quanpro288@gmail.com>
2021-12-29 01:22:11 +0700

    personal kpi create

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,55 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Comment.css";
+import COMMENTS from "../../fake-data/comments";
+
+const CommentItem = (props) => {
+  const { profileUrl, username, text, timestamp } = props;
+  return (
+    <div className='comment__item__container'>
+      <div className='comment__item__profile'>
+        <img src={profileUrl} className='comment__profile__img' alt='Profile' />
+      </div>
+      <div className='comment__item__content'>
+        <div className='comment__author'>{username}</div>
+        <div className='comment__text'>{text}</div>
+        <div className='comment__timestamp'>{timestamp}</div>
+      </div>
+    </div>
+  );
+};
 
 const Comment = (props) => {
+  const [comments, setComments] = useState(COMMENTS);
+
   return (
     <div className='comment__container'>
-      <ul class='nav nav-tabs' id='myTab' role='tablist'>
-        <li class='nav-item'>
-          <a
-            class='nav-link active'
-            id='home-tab'
-            data-toggle='tab'
-            href='#comment'
-            role='tab'
-            aria-controls='home'
-            aria-selected='true'
-          >
-            Home
-          </a>
-        </li>
-        <li class='nav-item'>
-          <a
-            class='nav-link'
-            id='document-tab'
-            data-toggle='tab'
-            href='#document'
-            role='tab'
-            aria-controls='document'
-            aria-selected='false'
-          >
-            Tài liệu
-          </a>
-        </li>
-      </ul>
-      <div class='tab-content' id='myTabContent'>
-        <div
-          class='tab-pane fade show active'
-          id='comment'
-          role='tabpanel'
-          aria-labelledby='comment-tab'
-        >
-          ...
-        </div>
-        <div
-          class='tab-pane fade'
-          id='document'
-          role='tabpanel'
-          aria-labelledby='document-tab'
-        >
-          ...
-        </div>
-      </div>
+      {comments.map((comment) => (
+        <CommentItem
+          profileUrl={comment.profileUrl}
+          username={comment.username}
+          text={comment.text}
+          timestamp={comment.timestamp}
+        />
+      ))}
     </div>
   );
 };
--- src/containers/kpi-member/PersonalKPICreator/PersonalKPICreator.js
+++ src/containers/kpi-member/PersonalKPICreator/PersonalKPICreator.js
@@ -1,25 +1,126 @@
-import React, { useState } from "react";
-import Comment from "../../../components/Comment/Comment";
+import React, { useState, useEffect } from "react";
+import { Dropdown } from "semantic-ui-react";
+import MOCK_DATA from "../../../components/Table/MOCK.json";
+import Table from "../../../components/Table/Table";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import GeneralModal from "../../../components/GeneralModal/GeneralModal";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./PersonalKPICreator.css";
+import Comment from "../../../components/Comment/Comment";
 
-export default function PersonalKPICreator() {
-  const [modalVisible, setModalVisible] = useState(false);
+const managers = [
+  {
+    key: 1,
+    value: "Hoàng Huy Quân",
+    text: "Hoàng Huy Quân",
+  },
+  { key: 2, value: "Nguyễn Đức Thắng", text: "Nguyễn Đức Thắng" },
+  { key: 3, value: "Nguyễn Đình Hùng", text: "Nguyễn Đình Hùng" },
+  { key: 4, value: "Đỗ Hữu Bằng", text: "Đỗ Hữu Bằng" },
+];
+
+export default function UnitKPICreator() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [showModal, setShowModal] = useState(false);
+  const [kpiData, setKpiData] = useState([]);
+
+  useEffect(() => {
+    const data = MOCK_DATA.filter((elm) => {
+      console.log(parseInt(elm["month"]));
+      console.log(parseInt(startDate.getMonth()), "get date");
+      return parseInt(elm["month"]) === parseInt(startDate.getMonth() + 1);
+    });
+
+    setKpiData(data.length > 0 ? data[0].data : []);
+  }, [startDate]);
+
+  const onSubmit = () => {
+    setShowModal(false);
+    toast("Gửi yêu cầu thành công", {
+      type: "success",
+      theme: "colored",
+    });
+  };
+
   return (
-    <div>
-      <h2>Khởi tạo KPI cá nhân</h2>
-      <button className='btn btn-primary' onClick={() => setModalVisible(true)}>
-        Khởi tạo KPI tháng 12
-      </button>
+    <div className='personal__create__container'>
+      <ToastContainer />
       <GeneralModal
-        show={modalVisible}
-        closeHandler={() => setModalVisible(false)}
+        show={showModal}
+        closeHandler={() => {
+          setShowModal(false);
+        }}
       >
-        <p>
-          Đơn vị cấp trên của bạn chưa thiếp lập KPI. Liên hệ với trưởng đơn vị
-          để yêu cầu thiết lập
-        </p>
+        <div className='mb-3'>
+          <b>
+            Đơn vị cấp trên của bạn chưa thiết lập KPI. Liên hệ với trưởng đơn
+            vị để yêu cầu khởi tạo
+          </b>
+        </div>
+        <div className='mb-5'>
+          <Dropdown
+            placeholder='Chọn cấp trên'
+            fluid
+            search
+            selection
+            options={managers}
+          />
+        </div>
+        <div className='modal-toolbox'>
+          <button class='btn btn-danger' onClick={() => setShowModal(false)}>
+            Huỷ
+          </button>
+          <button class='btn btn-success ml-4' onClick={onSubmit}>
+            Gửi yêu cầu
+          </button>
+        </div>
       </GeneralModal>
-      <Comment />
+
+      <div className='mb-5'>
+        <div>
+          <label>Tháng</label>
+          <DatePicker
+            dateFormat='MM/yyyy'
+            selected={startDate}
+            showMonthYearPicker
+            onChange={(date) => setStartDate(date)}
+          />
+        </div>
+      </div>
+
+      <div className='personal__init_container'>
+        {startDate.getMonth() > 10 || startDate.getFullYear() > 2021 ? (
+          <div>
+            <div className='init__option__group mb-5'>
+              <button
+                className='btn btn-primary mr-3'
+                onClick={() => setShowModal(true)}
+              >
+                Khởi tạo KPI tháng 12
+              </button>
+              <button
+                className='btn btn-primary'
+                onClick={() => setShowModal(true)}
+              >
+                Sao chép KPI đơn vị cha
+              </button>
+            </div>
+            <div>
+              <h2>KPI cá nhân tháng 12-2021</h2>
+              <p>Chưa khởi tạo KPI tháng 12-2021</p>
+            </div>
+          </div>
+        ) : (
+          kpiData.length > 0 && (
+            <div>
+              <Table data={kpiData} rowPerPage={5} rowClick={() => {}} />
+              <Comment />
+            </div>
+          )
+        )}
+      </div>
     </div>
   );
 }
