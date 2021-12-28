c6a502e2c999666728326ccc9e175d7192491beb
NguyenDucThangNghiemTuc <thang.nd182629@gmail.com>
2021-12-28 12:01:39 +0700

    add fake data

---
+++ src/fake-data/kpi-target.js
@@ -0,0 +1,31 @@
+const KPI_TARGET = [
+  {
+    "Tên mục tiêu": "Phê duyệt công việc",
+    "Mục tiêu cha": "Phê duyệt công việc",
+    "Tiêu chí đánh giá":
+      "Thực hiện tốt vai trò người phê duyệt trong các công việc. Người phê duyệt là người chịu trách nhiệm về thành công/thất bại của công việc",
+    "Trọng số": 5,
+  },
+  {
+    "Tên mục tiêu": "Phê duyệt công việc",
+    "Mục tiêu cha": "Phê duyệt công việc",
+    "Tiêu chí đánh giá":
+      "Thực hiện tốt vai trò người phê duyệt trong các công việc. Người phê duyệt là người chịu trách nhiệm về thành công/thất bại của công việc",
+    "Trọng số": 5,
+  },
+  {
+    "Tên mục tiêu": "Phê duyệt công việc",
+    "Mục tiêu cha": "Phê duyệt công việc",
+    "Tiêu chí đánh giá":
+      "Thực hiện tốt vai trò người phê duyệt trong các công việc. Người phê duyệt là người chịu trách nhiệm về thành công/thất bại của công việc",
+    "Trọng số": 5,
+  },
+  {
+    "Tên mục tiêu": "Phê duyệt công việc",
+    "Mục tiêu cha": "Phê duyệt công việc",
+    "Tiêu chí đánh giá":
+      "Thực hiện tốt vai trò người phê duyệt trong các công việc. Người phê duyệt là người chịu trách nhiệm về thành công/thất bại của công việc",
+    "Trọng số": 5,
+  },
+]
+export default KPI_TARGET
